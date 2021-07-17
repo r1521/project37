64199cef0e22df5cd0d8ecf0cf5a747d75a7942f
r1521 <78888719+r1521@users.noreply.github.com>
2021-07-16 20:46:33 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var canva;
+var gameState, contestantCount, database, quiz, question, contestant;
+var allContestants;
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+  quiz = new Quiz();
+  quiz.getState();
+  quiz.start();
+ 
+}
+
+
+function draw(){
+  background("pink");
+
+  
+}
